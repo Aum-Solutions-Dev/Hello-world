d55ef1d2b2d08821f03a095b9e822e9357b9a9ab
Aum-Solutions-Dev <aumsolutionsdev@gmail.com>
2025-05-23 12:29:49 +0530

    added the same line for both branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello and welcome !!");
         System.out.println("Main branch2");
         System.out.println("Git Main2 branch");
+        System.out.println("This is the same line for both branch.");
     }
 }
